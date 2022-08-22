9d774791c391056626dad63f8f4a5febe483e774
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-22 20:40:50 -0300

    Aula 2 - comando print

---
+++ aula2/aula2.py
@@ -0,0 +1,11 @@
+#aula sobre o comando print
+print("aula 2 - comando print")
+
+print('johnatan' , 'Paixão')
+print('johnatan' , 'Paixão', sep='-')
+print('johnatan' , 'Paixão', sep='-', end=' - ')
+print('johnatan' , 'Paixão', sep='-')
+
+print('desafio! - print no cpf')
+print('111' , '222', '333', sep='.', end='-')
+print('44')
