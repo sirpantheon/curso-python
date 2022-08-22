520ba497f93e3fabbd798dc55048d986b1d9b02a
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-22 19:52:48 -0300

    Aula 2 - comentarios

---
+++ aula1.py
@@ -0,0 +1,11 @@
+#aula 1 - comentarios
+print('Aula sobre comentarios!')
+print("-----------------------")
+print("linha 4")
+print("linha 5")
+print("linha 6")
+print("linha 7")
+print("linha 8")
+"""
+    isso e um comentario multilinhas
+"""
