eaa8c8b6a4b9a36d2057d0a473a85518a7cae8e7
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-23 18:35:16 -0300

    aula 7 - entradas de dados

---
+++ aula 7/aula7.py
@@ -0,0 +1,8 @@
+#aula 7 - entradas de dados
+
+nome = input("qual e seu nome? ")
+sobreNome = input("qual e seu sobrenome? ")
+idade = input("qual e sua idade? ")
+dataNascimento = 2022 - int(idade)
+
+print(f"meu nome e {nome} {sobreNome} e eu nasci em {dataNascimento}")
