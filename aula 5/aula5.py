29b9f9a121cafe4db62f517bb38a5b51459bc566
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-22 23:31:35 -0300

    aula 5 - variaveis

---
+++ aula 5/aula5.py
@@ -0,0 +1,16 @@
+# aula 5 - variaveis
+
+nome = "johnatan"
+sobreNome = "paixão"
+idade = 27
+altura = 1.70
+peso = 77
+maiorIdade = idade > 18
+imc = peso / (altura ** 2)
+
+print("nome: ", nome, sobreNome)
+print("idade: ", idade)
+print("altura: ", altura)
+print("E maior de idade?: ", maiorIdade)
+print("imc de ",nome," e ", imc)
+
