ce5981b58d4bc6ebe325f8dea7d1066391497c72
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-22 22:59:59 -0300

    aula 4 - primitivos

---
+++ aula4/aula4.py
@@ -0,0 +1,18 @@
+# aula 4 - tipos de dados
+print('johnatan' , type("johnatan"))
+print(132 , type(123))
+print(132.3 , type(123.3))
+print(10 == 10, type(10 == 10))
+
+print('johnatan' , type("johnatan"), bool("johnatan"))
+
+#desafio
+
+print("nome")
+print("johnatan", type("johnatan"))
+
+print("idade")
+print(27, type(27))
+
+print("altura")
+print(1.70, type(1.70))
