fd7d46f192aee17e459f28d82ba2e9556dde727e
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-23 23:52:17 -0300

    aula 8 - if/else/elif

--- aula3/aula3.py
+++ aula3/aula3.py
@@ -1,5 +1,5 @@
 #aula 3 - Strings
 
-print("isso e uma 'string' (str)"),
-print("isso e uma \'string\' (str)"),
-print("isso e uma quebra de linha \n 'usando \\n'"),
+print("isso e uma 'string' (str)")
+print("isso e uma \'string\' (str)")
+print("isso e uma quebra de linha \n 'usando \\n'")
